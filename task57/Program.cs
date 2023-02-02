7194f5917a574155dc3719fdc4379c13a791d276
Alsu <magadieva.alsu@yandex.ru>
2023-02-02 21:20:39 +0500

    Добавили 53, 55, 57, 59

---
+++ task57/Program.cs
@@ -0,0 +1,87 @@
+﻿// Составить частотный словарь элементов двумерного массива. Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5},");
+            else Console.Write($"{matrix[i, j],5}  ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[] MatrixToArray (int [,] matrix)
+{
+    int[] arr = new int[matrix.Length];
+    int k = 0;
+for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            arr[k] = matrix[i,j];
+            k++;
+        }
+    }
+    return arr;
+}
+
+void PrintArray(int [] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+}
+
+void UniqElemQuantity (int[] arr)
+{
+    int number = arr[0];
+    int count = 1;
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i] == number) count++;
+        else
+        {
+            Console.WriteLine($"{number} -> {count}");
+            number = arr[i];
+            count = 1;
+        }
+    }
+    Console.WriteLine($"{number} -> {count}");
+}
+
+
+int[,] array2D = CreateMatrixRndInt(5, 4, 1, 10);
+PrintMatrix(array2D);
+Console.WriteLine();
+
+int[] array = MatrixToArray(array2D);
+PrintArray(array);
+Console.WriteLine();
+
+Array.Sort(array);
+PrintArray(array);
+
+Console.WriteLine();
+UniqElemQuantity(array);
---
+++ task59/Program.cs
@@ -0,0 +1,89 @@
+﻿// Задайте двумерный массив из целых чисел. Напишите программу, которая удалит строку и столбец, на пересечении которых расположен наименьший элемент массива.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5},");
+            else Console.Write($"{matrix[i, j],5}  ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[] IndexMinVal(int[,] matrix)
+{
+    int min = matrix[0, 0];
+    int indexRow = 0;
+    int indexColumn = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] < min)
+            {
+                min = matrix[i, j];
+                indexRow = i;
+                indexColumn = j;
+            }
+        }
+
+    }
+    return new int[] { indexRow, indexColumn, min };
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+}
+
+int[,] RemoveCrossRowColumns(int[,] matrix, int removeRow, int removeColumn)
+{
+    int[,] newMatrix = new int[matrix.GetLength(0) - 1, matrix.GetLength(1) - 1];
+    int m = 0;
+    int n = 0;
+    for (int i = 0; i < newMatrix.GetLength(0); i++)
+    {
+        if (i == removeRow) m++;
+        for (int j = 0; j < newMatrix.GetLength(1); j++)
+        {
+            if (j == removeColumn) n++;
+            newMatrix[i, j] = matrix[m,n];
+            n++;
+        }
+        m++;
+        n = 0;
+    }
+    return newMatrix;
+}
+
+int[,] array2D = CreateMatrixRndInt(5, 4, -10, 10);
+PrintMatrix(array2D);
+Console.WriteLine();
+int[] indexMinVal = IndexMinVal(array2D);
+PrintArray(indexMinVal);
+Console.WriteLine();
+int[,] removeCrossRowColumns = RemoveCrossRowColumns(array2D, indexMinVal[0], indexMinVal[1]);
+PrintMatrix(removeCrossRowColumns);
